04bbb618f9707a402efd78b7ad6274897d173733
Mark Ratjens <mark@ratjens.com>
2019-03-18 13:43:20 +1100

    Initial Commit

---
+++ Bamal/Bamal.h
@@ -0,0 +1,19 @@
+//
+//  Bamal.h
+//  Bamal
+//
+//  Created by Mark Ratjens on 18/3/19.
+//  Copyright © 2019 Nyasa Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Bamal.
+FOUNDATION_EXPORT double BamalVersionNumber;
+
+//! Project version string for Bamal.
+FOUNDATION_EXPORT const unsigned char BamalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bamal/PublicHeader.h>
+
+
